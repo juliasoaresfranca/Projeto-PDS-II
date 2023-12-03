1b5ac7242c3ec83c7c5f2a3f3b1fbae42b744e64
olivermggomes <148919955+olivermggomes@users.noreply.github.com>
2023-12-02 22:49:07 -0300

    Update ListaMidia.cpp

--- Código/ListaMidia.cpp
+++ Código/ListaMidia.cpp
@@ -1,67 +1,156 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
+#include "ListaMidia.h"
+#include <algorithm>
 
-class ListaMidia {
-private:
-    std::unordered_map<std::string, Midia> listaDeMidias;
+std::string ListaMidia::toString() const
+{
+    std::string retorno = "\n";
 
-public:
-    ListaMidia() {}
+    if(!listaDeMidias.empty()){
+    for (const auto& entry : listaDeMidias) {
+        const Midia& atual = entry.second;
+        retorno += atual.descricao();
+    }
+    }
 
-    std::string toString() {
-        std::string retorno = "";
+    return retorno + "\n";
+}
 
+std::string ListaMidia::toStringAvaliacoes() {
+    std::string retorno = "\n";
+
+    if (!listaDeMidias.empty()) {
         for (const auto& entry : listaDeMidias) {
             const Midia& atual = entry.second;
-            retorno += "\n Nome: " + atual.nome + " || Identificação: " + atual.ID + "\n";
-        }
+            retorno += atual.descricao();
 
-        return retorno;
-    }
-
-    bool isVazia() {
-        return listaDeMidias.empty();
-    }
-
-    Midia* Buscar(const std::string& ID) {
-        if (listaDeMidias.find(ID) != listaDeMidias.end()) {
-            return &listaDeMidias[ID];
-        }
-        return nullptr;
-    }
-
-    int tamanhoLista() {
-        return listaDeMidias.size();
-    }
-
-    std::vector<Midia> Buscar(const std::string& nome, const std::string& idioma, const std::string& genero, const std::string& id) {
-        std::vector<Midia> ListaRetorno;
-
-        for (const auto& entry : listaDeMidias) {
-            const Midia& analisada = entry.second;
-            bool nomeMatch = nome.empty() || analisada.nome.find(nome) != std::string::npos;
-            bool idiomaMatch = idioma.empty() || analisada.idioma.find(idioma) != std::string::npos;
-            bool generoMatch = genero.empty() || analisada.genero.find(genero) != std::string::npos;
-            bool idMatch = id.empty() || analisada.ID == id;
-
-            if (nomeMatch && idiomaMatch && generoMatch && idMatch) {
-                ListaRetorno.push_back(analisada);
+            if (!atual.listaDeAvaliacoes.empty()) {
+                retorno += "\n Avaliações:\n";
+                for (const Avaliacao& avaliacao : atual.listaDeAvaliacoes) {
+                    retorno += " - Nota: " + std::to_string(avaliacao.nota) + "\n";
+                }
             }
+            retorno += "\n";
         }
-
-        return ListaRetorno;
     }
 
-    void AdicionarMidia(const std::string& Id, const Midia& Adicionada) {
-        listaDeMidias[Id] = Adicionada;
+    return retorno;
+}
+
+
+bool ListaMidia::isVazia() const
+{
+    return listaDeMidias.empty();
+}
+
+Midia ListaMidia::Buscar(const std::string& ID) {
+    auto it = listaDeMidias.find(ID);
+
+    if (it != listaDeMidias.end()) {
+        // Elemento encontrado, retorna o valor associado à chave
+        return it->second;
+    }
+    else {
+        throw std::runtime_error("Elemento nao encontrado para a chave fornecida.");
+    }
+}
+
+int ListaMidia::tamanhoLista() const
+{
+    return listaDeMidias.size();
+}
+
+std::vector<Midia> ListaMidia::Buscar(const std::string& nome, const std::string& idioma, const std::string& genero, const std::string& id) const
+{
+    std::vector<Midia> ListaRetorno;
+
+    for (const auto& entry : listaDeMidias) {
+        const Midia& analisada = entry.second;
+        bool nomeMatch = nome.empty() || analisada.nome.find(nome) != std::string::npos;
+        bool idiomaMatch = idioma.empty() || analisada.idioma.find(idioma) != std::string::npos;
+        bool generoMatch = genero.empty() || analisada.genero.find(genero) != std::string::npos;
+        bool idMatch = id.empty() || analisada.ID == id;
+
+        if (nomeMatch && idiomaMatch && generoMatch && idMatch) {
+            ListaRetorno.push_back(analisada);
+        }
     }
 
-    void RemoverMidia(const std::string& Id) {
-        listaDeMidias.erase(Id);
+    return ListaRetorno;
+
+}
+
+
+void ListaMidia::AdicionarMidia(const std::string& Id, std::string nome, std::string idioma, std::string genero, std::string data)
+{
+
+    Midia midiaAdicionada(nome,idioma,genero,data,Id);
+
+    listaDeMidias.insert(std::make_pair(Id, midiaAdicionada));
+
+}
+
+void ListaMidia::AdicionarSerie(const std::string& Id, std::string nome, std::string idioma, std::string genero, std::string data,int episodios)
+{
+
+    Serie serie = Serie::criarSerie(nome, idioma, genero, data, Id, episodios);
+
+    listaDeMidias.insert(std::make_pair(Id, serie));
+}
+
+void ListaMidia::AdicionarFilme(const std::string& Id, std::string nome, std::string idioma, std::string genero, std::string data,float duracao)
+{
+
+    Filme Filme = Filme::criarFilme(nome, idioma, genero, data, Id, duracao);
+    listaDeMidias.insert(std::make_pair(Id, Filme));
+}
+
+void ListaMidia::RemoverMidia(const std::string& Id)
+{
+
+    listaDeMidias.erase(Id);
+
+}
+
+bool ListaMidia::Contem(const std::string& Id) const
+{
+    return listaDeMidias.find(Id) != listaDeMidias.end();
+}
+
+std::list<Midia> ListaMidia::top10Midias()
+{
+    std::vector<Midia> tempVector;
+
+    // Preencha o vetor temporário com as mídias do mapa
+    for (const auto& pair : listaDeMidias) {
+        tempVector.push_back(pair.second);
     }
 
-    bool Contem(const std::string& Id) {
+    // Ordene o vetor com base no método mediaAvaliacao
+    std::sort(tempVector.begin(), tempVector.end(), [](const Midia& a, const Midia& b) {
+        // Substitua isso pela lógica real de comparação
+        return a.mediaAvaliacao() > b.mediaAvaliacao();
+        });
+
+    std::list<Midia> top10List;
+
+    int count = 0;
+    for (const Midia& midia : tempVector) {
+        top10List.push_back(midia);
+        count++;
+
+        if (count == 10) {
+            break;
+        }
+    }
+
+    return top10List;
+}
+
+void ListaMidia::AdcionarMidiaExistente(Midia adicionada)
+{
+    listaDeMidias.insert(std::make_pair(adicionada.ID, adicionada));
+}
+
         return listaDeMidias.find(Id) != listaDeMidias.end();
     }
 
